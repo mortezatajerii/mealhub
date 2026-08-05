922f0230beddcbf9ba0d3b467efb23f885047a0b
Morteza Tajerii <mortezatajerii@gmail.com>
2026-08-05 16:57:52 +0330

    🐞 fix(urls): home page
    adding url pattern of home page and linking it to index.html

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "index.html")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
